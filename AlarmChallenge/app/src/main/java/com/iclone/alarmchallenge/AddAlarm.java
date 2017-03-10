022624fca7a3348b76e60faffe364db66413e572
ThanThai21 <thaibk210@gmail.com>
2017-03-10 20:38:30 +0700

    16:38 10/2/2017

---
+++ AlarmChallenge/app/src/main/java/com/iclone/alarmchallenge/AddAlarm.java
@@ -0,0 +1,103 @@
+package com.iclone.alarmchallenge;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddAlarm extends AppCompatActivity {
+    ListView listView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_alarm);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        getSupportActionBar().setTitle("Alarm");
+        String[] day = new String[] {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        listView = (ListView) findViewById(R.id.list_view);
+        List<CustomLV> customLVs = new ArrayList<CustomLV>();
+        for (int i = 0; i < day.length; i++) {
+            CustomLV temp = new CustomLV();
+            temp.textDay = day[i];
+            customLVs.add(temp);
+        }
+
+        CustomAdapter customAdapter = new CustomAdapter(this, R.layout.custom_layout_listview, customLVs);
+        listView.deferNotifyDataSetChanged();
+        listView.setAdapter(customAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bar, menu);
+        return true;
+    }
+}
+
+class CustomLV {
+    String textDay;
+    public void  setTextDay(String textDay) {
+        this.textDay = textDay;
+    }
+
+    public String getTextDay() {
+        return textDay;
+    }
+
+    int idCheckbox;
+    public int getIdCheckbox() {
+        return idCheckbox;
+    }
+
+    public void setIdCheckbox(int idCheckbox) {
+        this.idCheckbox = idCheckbox;
+    }
+
+
+}
+
+class CustomAdapter extends ArrayAdapter {
+    Context context;
+    int resource;
+    List<CustomLV> objects;
+    public CustomAdapter(Context context, int resource, List objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.resource = resource;
+        this.objects = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(resource, parent, false);
+
+        TextView day = (TextView) rowView.findViewById(R.id.day_text);
+        CheckBox checkBox = (CheckBox) rowView.findViewById(R.id.checkbox);
+        day.setText(objects.get(position).getTextDay());
+        return rowView;
+    }
+}
